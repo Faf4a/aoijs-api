e4afe17bec93ae773fd320060e2d87ff45fd247b
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2024-04-16 18:36:44 +0530

    Create functionlist.js

---
+++ src/pages/api/v1/functionlist.js
@@ -0,0 +1,41 @@
+import functionData from "../../assets/data/functions.json";
+/**
+ * @swagger
+ * /api/v1/functionlist:
+ *   get:
+ *     summary: Get a list of all functions available in aoi.js
+ *     parameters:
+ *        none
+ *     responses:
+ *       200:
+ *         description: All fuunctions were sent
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 endpoint:
+ *                   type: string
+ *                 status:
+ *                   type: integer
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     function:
+ *                       type: string
+ *                     description:
+ *                       type: string
+ *                     example:
+ *                       type: string
+ */
+
+export default async function functionlist(req, res) {
+    var arr = [];
+    for (var i = 0; i < functionData.length; i++) {
+    	arr[i]= "$" + functionData[i].function;
+    }
+    
+    const json = JSON.stringify({ endpoint: "/functionlist", status: 200, data: {arr} }, null, 2);
+    res.setHeader("Content-Type", "application/json");
+    res.end(json);
+}
